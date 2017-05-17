7af1baabbe73dfefea705939d6894fbff012867d
Ben Smith <benjaminsmith1981@gmail.com>
2017-05-17 17:41:31 +0200

    Hello world app with view controller lifecycle print statements

--- HelloWorld2/HelloWorld2/ViewController.swift
+++ HelloWorld2/HelloWorld2/ViewController.swift
@@ -12,11 +12,37 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print("view did load")
+        
+    }
+    
+    override func loadView() {
+        super.loadView()
+        print("load view")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("View Will Appear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("View did Appear")
     }
 
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("will will disappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("view did disappear")
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        print("Did receive memory warning")
         // Dispose of any resources that can be recreated.
     }
 
